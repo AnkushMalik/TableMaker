a9013bdec347c2eac6d93d7779d1fadf94ff46e1
Ankush Malik <ankushmalik631@gmail.com>
2019-09-04 00:23:09 +0530

    Modify TableMaker

--- src/components/table-maker/table-maker.component.jsx
+++ src/components/table-maker/table-maker.component.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
 
 class TableMaker extends React.Component {
-    // constructor() {
-    //     super();
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            initialized: false,
+            tableDef: [{
+                "colDef": [],
+                "rowDef": []
+            }]
+        }
+    }
+    initializeTable = () => {
+        this.setState({ initialized: true });
+    }
+
     render() {
         return (
-            <div>
-                Table Component
+            <div className='tablemaker'>
+                <div className="initt">
+                    <button
+                        className='waves-effect waves-light btn'
+                        onClick={this.initializeTable}>
+                        Initialize Table
+                    </button>
+                </div>
+                {
+                    this.state.initialized ?
+                        (
+                            <button
+                                id='create_table'
+                                className='waves-effect waves-light btn'
+                                onClick={this.createTable}>
+                                Create
+                            </button>
+                        ) : null
+                }
             </div>
         )
     }
