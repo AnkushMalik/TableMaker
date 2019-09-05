92ed2e00b4c3f79c275a9d14b057e9b2f633b6b9
Ankush Malik <ankushmalik631@gmail.com>
2019-09-05 13:36:17 +0530

    Updates

---
+++ precache-manifest.b1e6148c73dc30eca6e5dc18e9d1b57b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f92f96c6ef6c9c5d1746b3c097e00f",
+    "url": "/TableMaker/index.html"
+  },
+  {
+    "revision": "b36ddd5c8623dafc1bfb",
+    "url": "/TableMaker/static/css/main.fdab398c.chunk.css"
+  },
+  {
+    "revision": "ac8dc878f4058655b357",
+    "url": "/TableMaker/static/js/2.ff68ce8c.chunk.js"
+  },
+  {
+    "revision": "b36ddd5c8623dafc1bfb",
+    "url": "/TableMaker/static/js/main.2919f6b1.chunk.js"
+  },
+  {
+    "revision": "973ae4c8efab4d64e11d",
+    "url": "/TableMaker/static/js/runtime~main.22e833c3.js"
+  }
+]);
